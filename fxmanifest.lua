adb16c20c4f0667b9db5dcdd16e435efd6dbabb2
GodsDevelopment69 <116272690+GodsDevelopment69@users.noreply.github.com>
2022-10-21 22:56:54 +0530

    Added Everything!!!

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '!Baap'
+description 'Piggyback'
+
+
+
+
+client_scripts {
+    'client/main.lua',
+
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+
+lua54 'yes'
